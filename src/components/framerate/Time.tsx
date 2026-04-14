00d1ace949b0cb6895f427cf0942e400795c56db
tsClauz <74681599+tsClauz@users.noreply.github.com>
2026-04-14 19:15:46 +0000

    fix millisecond slicing

--- src/components/framerate/Time.tsx
+++ src/components/framerate/Time.tsx
@@ -5,7 +5,7 @@ import toHHMMSS from './toHHMMSS'
 export default class extends React.Component {
     state: { framerate: string, framecount: string, time: string }
     props: { framerate?: string, framecount?: string }
-    
+
     constructor(props: { framerate?: string, framecount?: string }) {
         super(props)
         this.props = props
@@ -15,28 +15,28 @@ export default class extends React.Component {
 
     render() {
         return <>
-                <label>Framerate<br />
+            <label>Framerate<br />
 
-                <input type="number" className="input framerate" 
-                onChange={(event) => this.onChange('framerate', event)} 
-                placeholder="60" value={this.state.framerate.toString()} min="0" />
+                <input type="number" className="input framerate"
+                    onChange={(event) => this.onChange('framerate', event)}
+                    placeholder="60" value={this.state.framerate.toString()} min="0" />
 
-                </label>
-                <label>Framecount<br />
+            </label>
+            <label>Framecount<br />
 
-                <input type="number" className="input framecount" 
-                onChange={(event) => this.onChange('framecount', event)} 
-                placeholder="17868" value={this.state.framecount.toString()} min="0" />
+                <input type="number" className="input framecount"
+                    onChange={(event) => this.onChange('framecount', event)}
+                    placeholder="17868" value={this.state.framecount.toString()} min="0" />
 
-                </label>
-                <label>Time<br />{this.state.time}</label>
-            </>
+            </label>
+            <label>Time<br />{this.state.time}</label>
+        </>
     }
 
-    onChange(type: 'framerate'|'framecount', event: React.ChangeEvent<HTMLInputElement>) {
-        if(type === 'framerate') {
+    onChange(type: 'framerate' | 'framecount', event: React.ChangeEvent<HTMLInputElement>) {
+        if (type === 'framerate') {
             this.state = { ...this.state, framerate: event.target.value }
-        } else { 
+        } else {
             this.state = { ...this.state, framecount: event.target.value }
         }
         event.preventDefault()
@@ -45,15 +45,15 @@ export default class extends React.Component {
     }
 
     updateTime() {
-        if(!this.state.framecount || !this.state.framerate || 
+        if (!this.state.framecount || !this.state.framerate ||
             isNaN(parseFloat(this.state.framecount)) || isNaN(parseFloat(this.state.framerate)) ||
             parseFloat(this.state.framecount) < 1 || parseFloat(this.state.framerate) < 1
-            ) { 
+        ) {
             this.setState({ ...this.state, time: '00:00:00.000' })
-            return 
+            return
         }
         const seconds = (parseFloat(this.state.framecount.replaceAll(',', '.')) / parseFloat(this.state.framerate.replaceAll(',', '.')))
-        const milliseconds = (seconds - Math.floor(seconds)).toString().slice(2)
+        const milliseconds = (seconds - Math.floor(seconds)).toString().slice(0, 3)
         const time = `${toHHMMSS(toBigIntWithDecimal(seconds))}${milliseconds !== '' ? `.${milliseconds}` : ''}`
         this.state.time = time
         this.setState(this.state)
